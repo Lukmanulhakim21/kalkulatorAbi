a451ad5590863cfd8f7b2d0d2627a97bea59cb4e
Lukmanulhakim21 <lh336544@gmail.com>
2021-06-18 17:34:50 +0700

    kalkulator by abi

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <heading/>
+    <kalkulator/>
+  </div>
+</template>
+
+<script>
+import heading from './components/heading.vue'
+import kalkulator from './components/kalkulator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    heading,
+    kalkulator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/kalkulator.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="calc">
+        <div class="display">{{result || 0}}</div>
+        <div class="btn clear" @click="clear">C</div>
+        <div class="btn operator" @click="per">%</div>
+        <div class="btn" @click="append(7)">7</div>
+        <div class="btn " @click="append(8)">8</div>
+        <div class="btn" @click="append(9)">9</div>
+        <div class="btn operator" @click="divide">/</div>
+        <div class="btn" @click="append(4)">4</div>
+        <div class="btn" @click="append(5)">5</div>
+        <div class="btn" @click="append(6)">6</div>
+        <div class="btn operator" @click="multiply">x</div>
+        <div class="btn" @click="append(1)">1</div>
+        <div class="btn" @click="append(2)">2</div>
+        <div class="btn" @click="append(3)">3</div>
+        <div class="btn operator" @click="subtract">-</div>
+        <div class="btn" @click="append(0)">0</div>
+        <div class="btn" @click="dot">.</div>
+        <div class="btn" @click="equal">=</div>
+        <div class="btn operator" @click="sum">+</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            result: '',
+            prev: null,
+            operator: null,
+            operatorClick: false
+        }
+    },
+    methods: {
+        clear(){
+            this.result = '';
+        },
+        per(){
+            this.result = parseFloat(this.result)/100;
+        },
+        append(number){
+            if(this.operatorClick){
+                this.result = '';
+                this.operatorClick = false;
+            }
+            this.result = this.result+number;
+        },
+        dot(){
+            if(this.result.indexOf('.') === -1){
+                this.append('.');
+            }
+        },
+        divide(){
+            this.operator = (a,b) => b/a;
+            this.setPrev();
+        },
+        multiply(){
+            this.operator = (a,b) => b*a;
+            this.setPrev();
+        },
+        subtract(){
+            this.operator = (a,b) => b-a;
+            this.setPrev();
+        },
+        sum(){
+            this.operator = (a,b) => b+a;
+            this.setPrev();
+        },
+        equal(){
+            this.result = this.operator(
+                parseFloat(this.result),
+                parseFloat(this.prev)
+            );
+            this.prev=null;
+        },
+        setPrev(){
+            this.prev= this.result;
+            this.operatorClick = true;
+        }
+    }
+}
+</script>
+
+<style>
+    *{
+        background: #fe2c54
+    }
+    .calc{
+        width: 400px;
+        margin: 0 auto;
+        font-size: 40px;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(auto,auto);
+        border: 40px groove #aaa9ad;
+    }
+    .display{
+        grid-column: 1/5;
+        background: #333;
+        color: #fff;
+        padding: 20px;
+        text-align: right;
+    }
+    .clear{
+        grid-column: 1/4;
+    }
+    .btn{
+        padding: 10px;
+        background: linear-gradient(#fafafa, lightgrey);
+        border: 1px solid #999;
+        cursor: pointer;
+    }
+    .btn:active{
+        background: #777;
+        color: #fff;
+    }
+    .btn:hover{
+        background: #63ccc5;
+        color: #fff;
+    }
+    .operator{
+        color: #DC143C;
+    }
+</style>
